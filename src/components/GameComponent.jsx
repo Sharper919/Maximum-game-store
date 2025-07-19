a862f981237f6aa5eea9b7142d5ed7195e42340c
Sharper919 <maxberezyuk2005@gmail.com>
2025-07-19 12:28:26 +0300

    update 3 finally

--- src/components/GameComponent.jsx
+++ src/components/GameComponent.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './GameComponent.css';
 
-class GameComponent extends React.Component {
-    render() {
-        return (
-            <div className='game-component'>
-                <div className='game-image'>
-                    <img src={this.props.image} alt="" />
-                </div>
-                <span className='game-name'>{this.props.gameName}</span>
-                <span className='game-price'>{this.props.gamePrice}</span>
+const GameComponent = ({ image, gameName, gamePrice }) => {
+    const cardRef = useRef(null);
+
+    const handleClick = () => {
+        const card = cardRef.current;
+        if (card) {
+            card.classList.remove('pop-effect'); // скидаємо, якщо вже є
+            void card.offsetWidth; // перезапускаємо анімацію
+            card.classList.add('pop-effect');
+        }
+    };
+
+    return (
+        <div
+            ref={cardRef}
+            className="game-component"
+            onClick={handleClick}
+        >
+            <div className="game-image">
+                <img src={image} alt={gameName} />
             </div>
-        )
-    }
-}
+            <div className="game-name">{gameName}</div>
+            <div className="game-price">{gamePrice}</div>
+        </div>
+    );
+};
 
 export default GameComponent;
